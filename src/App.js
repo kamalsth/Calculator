e534ab40146da0b5f9b1c23679062ecf5aeabc8d
Kamal Shrestha <kamalshrestha583@gmail.com>
2023-05-18 19:39:33 +0545

    Adding calculation logic

--- src/App.js
+++ src/App.js
@@ -15,20 +15,114 @@ export const ACTIONS={
 function reducer(state,{type,payload}){
   switch(type){
     case ACTIONS.ADD_DIGIT:
+      if(state.overwrite){
+        return{
+          ...state,
+          currentOperand:payload.digit,
+          overwrite:false,
+        }
+      }
+      if(payload.digit==="0" && state.currentOperand==="0")return state //return current state
+      if(payload.digit==="." && state.currentOperand.includes("."))return state //return current state
       return {
         ...state,
         currentOperand: `${state.currentOperand  || ""}${payload.digit}` 
       }
-      case ACTIONS.CHOOSE_OPERATION:
+
+    case ACTIONS.CHOOSE_OPERATION:
+        if(state.currentOperand==null && state.previousOperand==null){
+          return state
+        }
+        if(state.currentOperand==null){
+          return{
+            ...state,
+            operation:payload.operation,
+          }
+        }
+        if(state.previousOperand==null){
+          return{
+            ...state,
+            operation:payload.operation,
+            previousOperand:state.currentOperand,
+            currentOperand:null,
+
+          }
+        }
+        return{
+          ...state,
+          previousOperand:evaluate(state),
+          operation:payload.operation,
+          currentOperand:null
+        }
+
+    case ACTIONS.DELETE_DIGIT:
+      if(state.overwrite){
+        return{
+          ...state,
+          overwrite:false,
+          currentOperand:null
+        }
+      }
+      if(state.currentOperand==null) {
+        return state
+      }
+      if(state.currentOperand.length===1) {
         return {
           ...state,
-          currentOperand: `${state.currentOperand  || ""}${payload.operation}` 
+          currentOperand:null
         }
+      }
+      return{
+        ...state,
+        currentOperand:state.currentOperand.slice(0,-1)
+      }
+       
+     
+    case ACTIONS.CLEAR:
+        return {}
+
+    
+    case ACTIONS.EVALUATE:
+        if(state.operation==null || state.currentOperand==null|| state.previousOperand==null){
+          return state
+        }
+        return{
+          ...state,
+          overwrite:true,
+          previousOperand:null,
+          operation:null,
+          currentOperand:evaluate(state),
+        }
+
       default:
        return 0;
   }
 }
 
+function evaluate({currentOperand,previousOperand,operation}){
+  const previous=parseFloat(previousOperand)
+  const current=parseFloat(currentOperand)
+  if(isNaN(previous)||isNaN(current)){
+    return ""
+  }
+  let computation=""
+  switch (operation){
+    case "+":
+      computation=previous+current
+      break
+    case "-":
+      computation=previous-current
+      break
+    case "*":
+      computation=previous*current
+      break
+    case "÷":
+      computation=previous / current
+      break
+  }
+  return computation.toString()
+
+}
 function App() {
 
   const  [{currentOperand,previousOperand,operation},dispatch]=useReducer(reducer,{});
@@ -40,8 +134,8 @@ function App() {
         <div className="previous-operand">{previousOperand} {operation}</div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
+      <button className="span-two" onClick={()=>dispatch({type:ACTIONS.CLEAR})}>AC</button>
+      <button onClick={()=>dispatch({type:ACTIONS.DELETE_DIGIT})}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch}/>
       <DigitButton digit="1" dispatch={dispatch}/>
       <DigitButton digit="2" dispatch={dispatch}/>
@@ -61,7 +155,7 @@ function App() {
       <DigitButton digit="." dispatch={dispatch}/>            
       <DigitButton digit="0" dispatch={dispatch}/>
 
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={()=>dispatch({type:ACTIONS.EVALUATE})}>=</button>
 
     </div>
   );
